7878e14e4dc2e59220b3e5550e6f48fa31e9a735
rafael.maier <rafael.maier@eemovel.com>
2024-04-09 14:19:42 -0300

    chore: env again

--- src/app/services/getHeroes.ts
+++ src/app/services/getHeroes.ts
@@ -2,9 +2,7 @@ import { Hero } from "@/interfaces/heroes";
 
 export const getHeroes = async (): Promise<{ data: Hero[] }> => {
   if (process.env.VERCEL_ENV === "production") {
-    const response = await fetch(
-      `https://${process.env.VERCEL_URL}/api/heroes`
-    );
+    const response = await fetch(`${process.env.DOMAIN_ORIGIN}/api/heroes`);
     if (!response.ok) {
       throw new Error("Failed to request heroes list");
     }
